7f034cfeb19f9d337424822cfe13a7c9ad61d83b
Susanne Wulz <susanne.wulz@student.tugraz.at>
2021-08-05 15:33:33 +0200

    changed name of simulation functions

--- main.py
+++ main.py
@@ -31,31 +31,23 @@ def random_init_samples(domain, n_trajectories: int):
     return values(n_trajectories)
 
 
-def simulate_numerically(y0s: torch.Tensor, number_of_steps: int, step_size: float) -> torch.Tensor:
+def simulate_ode(f, y0s: torch.Tensor, number_of_steps: int, step_size: float) -> torch.Tensor:
     time_points = torch.arange(0., step_size * (number_of_steps + 1), step_size)
     ys = [(odeint(f, y0, time_points)[1:]) for y0 in y0s]
     return torch.stack(ys).float()
 
 
-def f(t, y):
-    theta = y[0]
-    omega = y[1]
-    d_theta = omega
-    d_omega = - g / l * torch.sin(theta)
-    return torch.tensor([d_theta, d_omega])
-
-
-def simulate(model, y0s: torch.Tensor, number_of_steps: int) -> torch.Tensor:
-    ys = [model(y0s)]
+def simulate_direct(f, y0s: torch.Tensor, number_of_steps: int) -> torch.Tensor:
+    ys = [f(y0s)]
     for _ in range(number_of_steps):
-        ys.append(model(ys[-1]))
+        ys.append(f(ys[-1]))
     return torch.swapaxes(torch.stack(ys), 0, 1)
 
 
-def simulate_integr(model, y0s: torch.Tensor, number_of_steps: int, step_size: float) -> torch.Tensor:
-    ys = [y0s + step_size * model(y0s)]
-    for _ in range(number_of_steps):  #
-        ys.append(ys[-1] + step_size * model(ys[-1]))
+def simulate_euler(f, y0s: torch.Tensor, number_of_steps: int, step_size: float) -> torch.Tensor:
+    ys = [y0s + step_size * f(y0s)]
+    for _ in range(number_of_steps):
+        ys.append(ys[-1] + step_size * f(ys[-1]))
     return torch.swapaxes(torch.stack(ys), 0, 1)
 
 
@@ -67,6 +59,21 @@ if __name__ == '__main__':
     y0s_domain = [[-1., 1.], [-1., 1.]]
     grid_init_samples(y0s_domain, 10)
     y0s = torch.tensor(random_init_samples(y0s_domain, 1000))
+
+    number_of_steps_train = 1
+    step_size = 0.01
+
+
+    def f(t, y):
+        theta = y[0]
+        omega = y[1]
+        d_theta = omega
+        d_omega = - g / l * torch.sin(theta)
+        return torch.tensor([d_theta, d_omega])
+
+
+    x = y0s.float()
+    y = simulate_ode(f, y0s, number_of_steps_train, step_size)
 
     """
     Data model
@@ -82,17 +89,12 @@ if __name__ == '__main__':
         nn.Linear(size_of_hidden_layers, output_size),
     )
 
-    number_of_steps_train = 1
-    step_size = 0.01
-    x = y0s.float()
-    y = simulate_numerically(y0s, number_of_steps_train, step_size)
-
     epochs = 1000
     opt = torch.optim.Adam(model.parameters())
     progress = tqdm(range(epochs), 'Training')
     for _ in progress:
         # y_pred = simulate(model, x, n_training)
-        y_pred = simulate_integr(model, x, number_of_steps_train, step_size)
+        y_pred = simulate_euler(model, x, number_of_steps_train, step_size)
 
         loss = F.mse_loss(y_pred, y)
         loss.backward()
@@ -109,10 +111,11 @@ if __name__ == '__main__':
     number_of_steps_test = 5
     step_size = 0.1
     x = y0s.float()
-    y = simulate_numerically(y0s, number_of_steps_test, step_size)
+    y = simulate_ode(f, y0s, number_of_steps_test, step_size)
 
     # y_pred = simulate(model, n_test, x)
-    y_pred = simulate_integr(model, x, number_of_steps_test, step_size)
+    y_pred = simulate_euler(model, x, number_of_steps_test, step_size)
+    # y_pred = simulate_numerically(model, x, number_of_steps_test, step_size)
     loss = F.mse_loss(y_pred, y)
     print(f'Pred loss = {loss}')
 
